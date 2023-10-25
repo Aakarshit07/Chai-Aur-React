f2904c2296349a4f6ec17bae70ce48a18fa75db1
Aakarshit07 <aakarshit070@gmail.com>
2023-10-25 19:30:39 +0530

    "React Router DOM Project"

---
+++ react-router-dom/src/App.jsx
@@ -0,0 +1,15 @@
+import { Header, Home, Footer } from './Input/Input'
+import './App.css'
+
+
+function App() {
+  
+  return (
+    <>
+
+      Hello
+    </>
+  )
+}
+
+export default App
---
+++ react-router-dom/src/Input/Input.js
@@ -0,0 +1,9 @@
+import Header from "../components/Header/Header";
+import Home from "../components/Home/Home";
+import Footer from "../components/Footer/Footer";
+import About from "../components/About/About";
+import Contact from "../components/Contact/Contact";
+import User from "../components/User/User";
+import Github from "../components/GitHub/Github";
+
+export {Header, Home, Footer, About, Contact, User, Github} ;
