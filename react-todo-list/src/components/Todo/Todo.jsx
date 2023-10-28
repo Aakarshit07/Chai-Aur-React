99b96452bbc75a78a98d1345218a8c9fc5ad0109
Aakarshit07 <aakarshit070@gmail.com>
2023-10-28 23:56:35 +0530

    "Added context api"

--- react-todo-list/src/components/Todo/Todo.jsx
+++ react-todo-list/src/components/Todo/Todo.jsx
@@ -1,13 +1,27 @@
+import { useState } from "react"
 
 
 
-export function Todo({todoData}) {
+export function Todo({todoData, isFinished, changeFinished, onDelete, onEdit}) {
+
+    const [finished, setFinished]=useState(isFinished);
+    const [isEditing, setIsEditing]=useState(false);
+    const [editText, setEditText]=useState(todoData);
+
     return (
         <div>
-            <input type="checkbox"></input>
-            {todoData}
-            <button>Edit</button>
-            <button>Delete</button>
+            <input type="checkbox" checked={finished} onChange={(e) => {
+                setFinished(e.target.checked);
+                changeFinished(e.target.checked);
+            }} />
+
+            {(isEditing) ? <input type="text" value={editText} onChange={e => setEditText(e.target.value)} /> : <span>{todoData}</span>}
+            <button onClick={() => {
+                setIsEditing(!isEditing);
+                onEdit(editText);
+            }}> {(!isEditing) ? "Edit" : "Save"}</button>
+
+            <button onClick={onDelete}>Delete</button>
 
         </div>
     )
--- react-todo-list/src/components/TodoList/TodoList.jsx
+++ react-todo-list/src/components/TodoList/TodoList.jsx
@@ -1,15 +1,42 @@
 import { Todo } from "../Todo/Todo"
-
-
-function TodoList() {
-    const list = [
-        {id: 1, todoData: "todo 1"},
-        {id: 2, todoData: "todo 2"},
-    ];
-
+import TodoContext from "../../context/TodoContext";
+import { useContext } from "react";
+function TodoList({}) {
+    const  {list, setList} = useContext(TodoContext)
     return (
         <div>
-            {list.length > 0 && list.map(todo => <Todo key={todo.id} todoData={todo.todoData}/>)}
+            {list.length > 0 && 
+            list.map(todo => <Todo 
+                                key={todo.id}
+                                id={todo.id} 
+                                isFinished={todo.finished} 
+                                todoData={todo.todoData} 
+                                changeFinished={(isFinished) => {
+                                    console.log("is Finished",isFinished);
+                                    const updatedList = list.map(t => {
+                                        if(t.id == todo.id){
+                                            todo.finished = isFinished;
+                                        }
+                                        return t;
+                                    })
+                                    setList(updatedList);
+                                }}
+                                onDelete={() => {
+                                    const updatedList = list.filter(t => t.id != todo.id)
+                                    setList(updatedList)
+                                }}
+                                onEdit={(todoText) => {
+                                    const updatedList = list.map(t => {
+                                        if(t.id == todo.id) {
+                                            todo.todoData = todoText;
+                                        }
+                                        return t;
+                                    });
+                                    setList(updatedList);
+                                }}
+                            />)}
         </div>
     )
 }
+
+export default TodoList;
